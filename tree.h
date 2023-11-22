2bbeff40f7c5ef99d4bc2e330cea13ca33415533
Alex <alex.grabski555@gmail.com>
2023-11-22 21:57:11 +0100

    bolonesge

--- tree.h
+++ tree.h
@@ -15,19 +15,30 @@ class Tree {
         // Tree variables
         int tree_age;
         int tree_height;
+        double tree_sun_energy;
+        double tree_soil_minerals;
+        double tree_sun_energy_consumption;
+        bool dead;
 
         // Branch variables
+        int max_number_of_branches;
         int number_of_branches;
+        double combined_branches_length;
 
         // Leaf variables
+        int max_number_of_leaves;
         int number_of_leaves;
 
         // Root variables
-        
-
+        int number_of_roots;
+        double combined_roots_length;
 
     public:
+        Tree();
 
+        int getTreeAge();
+        int getTreeHeight();
+        double 
 };
 
 #endif
